1825d66317386b62cabf92bcd28703c3d9a98762
Pieter Belmans <pieterbelmans@gmail.com>
2023-07-31 09:20:47 +0200

    Adding the `setup.py` of the previous commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+        name="twisted",
+        version="1",
+        description="Compute twisted Hodge numbers of complete intersections",
+        url="https://github.com/pbelmans/twisted-hodge",
+        packages=setuptools.find_packages(),
+        zip_safe=False,
+)
